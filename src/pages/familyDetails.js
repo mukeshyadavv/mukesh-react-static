44cc7138b5e54f5713fcf07dd544187e903d050a
unknown <mail2mukeshyadav@gmail.com>
2022-09-20 12:36:07 +0530

    adding app to github

---
+++ src/pages/familyDetails.js
@@ -0,0 +1,219 @@
+import React from 'react'
+
+export default () => (
+  <div>
+    {/* 
+    <p>React Static is a progressive static site generator for React.</p>
+    <div class="wrapper">
+      <div class="box1">One</div>
+      <div class="box2">Two</div>
+      <div class="box3">Three</div>
+      <div class="box4">Four</div>
+      <div class="box5">Five</div>
+    </div>    
+    <h1>Greetings from the Yadav Family.</h1>
+    */}
+    <h3>Hover on each image to see the details about the person.</h3>
+    {/* */}
+    {/* */}
+    
+    <div class="wrapper">
+      <div className="container">    
+          <img src="pics/papa.jpg" alt="Papa" className="image"></img>
+            <div className="middle">
+              <div className="text">
+                <h2>Sube Singh Yadav</h2>
+                <p><b>DOB:</b> 13-Jun-1950</p>
+                <p><b>Education:</b> MA[English, History]</p>
+                <p><b>Occupation:</b> Retired School Principal</p>
+                <p><b>Interests:</b> Listening to Radio, exercising daily, minimalist life.</p>
+              </div>
+            </div>
+      </div>
+      <div className="container">        
+        <img src="pics/mummy.jpg" alt="Mummy" className="image">
+          </img>
+          <div className="middle">
+            <div className="text">
+              <h2>Meena Yadav</h2>
+              <p><b>DOB:</b> 14-07-1954</p>
+              <p><b>Education:</b> Primary School</p>
+              <p><b>Occupation:</b> HouseWife</p>
+              <p><b>Interests:</b> Watching TV, cooking, chinese food.</p>
+            </div>
+          </div>
+      </div>
+      <div className="container">        
+        <img src="pics/suman3.png" alt="Suman" className="image">
+          </img>
+          <div className="middle">
+            <div className="text">
+              <h2>Suman Yadav</h2>
+              <p><b>DOB:</b> 14-02-1973</p>
+              <p><b>Education:</b> MA(History), B.Ed</p>
+              <p><b>Occupation:</b> Principal, Govt School, Delhi</p>
+              <p><b>Interests:</b> Cooking, gardening, teaching.</p>
+            </div>
+          </div>
+      </div>
+      <div className="container">        
+        <img src="pics/pintoo.png" alt="Pintoo" className="image">
+          </img>
+          <div className="middle">
+            <div className="text">
+              <h2>Pintoo Yadav</h2>
+              <p><b>DOB:</b> 22-02-1971</p>
+              <p><b>Education:</b> MBA(Marketing)</p>
+              <p><b>Occupation:</b> Reginal Head(North), Lupin India</p>
+              <p><b>Interests:</b> Travelling, gardening, cricket.</p>
+            </div>
+          </div>
+      </div>
+      <div className="container">        
+        <img src="pics/eshita2.png" alt="Eshita" className="image">
+          </img>
+          <div className="middle">
+            <div className="text">
+              <h2>Eshita Yadav</h2>
+              <p><b>DOB:</b> 22-12-1999</p>
+              <p><b>Education:</b> B.Tech(CSE), DTU</p>
+              <p><b>Occupation:</b> Software Engineer(Zomato)</p>
+              <p><b>Interests:</b> Travelling, athletics, watching web-series.</p>
+            </div>
+          </div>
+      </div>
+      <div className="container">        
+        <img src="pics/eshita2.png" alt="Eshaan" className="image">
+          </img>
+          <div className="middle">
+            <div className="text">
+              <h2>Eshaan Yadav</h2>
+              <p><b>DOB:</b> 23-07-2001</p>
+              <p><b>Education:</b> Final Year Student at Delhi Technological University(DTU)</p>
+              <p><b>Occupation:</b> Student</p>
+              <p><b>Interests:</b> Software Programming, travelling, gadgets.</p>
+            </div>
+          </div>
+      </div>
+      <div className="container">        
+        <img src="pics/sunita_family_2.jpg" alt="Sunita" className="image">
+          </img>
+          <div className="middle">
+            <div className="text">
+              <h2>Sunita Yadav</h2>
+              <p><b>DOB:</b> 08-03-1976</p>
+              <p><b>Education:</b> M.Sc, B.Ed, M.Phil(Physics)</p>
+              <p><b>Occupation:</b> Assistant Professor(Physics), Daman</p>
+              <p><b>Interests:</b> Meeting new people, outdoor activities, cooking, cycling.</p>
+            </div>
+          </div>
+      </div>
+      <div className="container">        
+        <img src="pics/atul.jpg" alt="Atul" className="image">
+          </img>
+          <div className="middle">
+            <div className="text">
+              <h2>Atul Singh</h2>
+              <p><b>DOB:</b> 30-01-1973</p>
+              <p><b>Education:</b> B.Tech(AMU), EPBM(IIM Kolkata)</p>
+              <p><b>Occupation:</b> Vice President, Suzlon Energy</p>
+              <p><b>Interests:</b> Spritualism, meditation, BrahmKumaris, travelling.</p>
+            </div>
+          </div>
+      </div>
+      <div className="container">        
+        <img src="pics/ashmit.jpg" alt="Ashmit" className="image">
+          </img>
+          <div className="middle">
+            <div className="text">
+              <h2>Ashmit Singh</h2>
+              <p><b>DOB:</b> 08-11-2001</p>
+              <p><b>Education:</b> B.Tech Final Year(IIT Delhi)</p>
+              <p><b>Occupation:</b> Student</p>
+              <p><b>Interests:</b> Travelling, Basketball, Badmintion, TableTennis.</p>
+            </div>
+          </div>
+      </div>
+      <div className="container">        
+        <img src="pics/mukesh.jpg" alt="Mukesh" className="image">
+          </img>
+          <div className="middle">
+            <div className="text">
+              <h2>Mukesh Yadav</h2>
+              <p><b>DOB:</b> 29-10-1978</p>
+              <p><b>Education:</b> M.Tech(CSE), NIT Kurukshetra</p>
+              <p><b>Occupation:</b> Solutions Consultant(IT)</p>
+              <p><b>Interests:</b> Loves running, cycling, swimming, badminton.</p>
+            </div>
+          </div>
+      </div>
+      <div className="container">        
+        <img src="pics/sharmila.jpg" alt="Sharmila" className="image" width="500px" height="600px">
+          </img>
+          <div className="middle">
+            <div className="text">
+              <h2>Sharmila Yadav</h2>
+              <p><b>DOB:</b> 16-07-1980</p>
+              <p><b>Education:</b> P.hd(Chemistry), M.Phil</p>
+              <p><b>Occupation:</b> Associate Professor, Delhi University</p>
+              <p><b>Interests:</b> Dancing, cooking, chinese food.</p>
+            </div>
+          </div>
+      </div>
+      <div className="container">        
+        <img src="pics/aarav2.jpg" alt="Aarav" className="image">
+          </img>
+          <div className="middle">
+            <div className="text">
+              <h2>Aarav Yadav</h2>
+              <p><b>DOB:</b> 01-10-2007</p>
+              <p><b>Education:</b> 10th Standard Student</p>
+              <p><b>Occupation:</b> Student</p>
+              <p><b>Interests:</b> Italian Food, Travelling, urban designing, metro system(around the world).</p>
+            </div>
+          </div>
+      </div>
+      <div className="container">        
+        <img src="pics/dinesh_family.jpg" alt="Dinesh" className="image">
+          </img>
+          <div className="middle">
+            <div className="text">
+              <h2>Dinesh Yadav</h2>
+              <p><b>DOB:</b> 26-09-1980</p>
+              <p><b>Education:</b> MBA, B.Tech(NIT Kurukshetra)</p>
+              <p><b>Occupation:</b> DGM, Reliance Energy, Delhi</p>
+              <p><b>Interests:</b> Watching movies, trying new dishes, cricket.</p>
+            </div>
+          </div>
+      </div>
+      <div className="container">        
+        <img src="pics/swati.jpg" alt="Swati" className="image">
+          </img>
+          <div className="middle">
+            <div className="text">
+              <h2>Swati Yadav</h2>
+              <p><b>DOB:</b> 14-07-1984</p>
+              <p><b>Education:</b> MBA, B.Ed</p>
+              <p><b>Occupation:</b> School Teacher, Delhi</p>
+              <p><b>Interests:</b> Travelling, music, reading novels.</p>
+            </div>
+          </div>
+      </div>
+      <div className="container">        
+        <img src="pics/mrinank.jpg" alt="Mrinank" className="image">
+          </img>
+          <div className="middle">
+            <div className="text">
+              <h2>Mrinank Yadav</h2>
+              <p><b>DOB:</b> 15-01-2010</p>
+              <p><b>Education:</b> 7th Standard Student</p>
+              <p><b>Occupation:</b> Student</p>
+              <p><b>Interests:</b> Watching vlogs on tv, music, dancing.</p>
+            </div>
+          </div>
+      </div>
+
+
+      </div>
+  </div>
+)
---
+++ src/pages/index.js
@@ -0,0 +1,177 @@
+import React from 'react'
+// import family from "../../public/pics/family.jpg";
+
+export default () => (
+  <div style={{ textAlign: 'center' }}>
+    {/* */}
+    {/* */}
+    {/*<h1>Welcome to React-Static</h1>*/}
+    {/* 
+
+    {familyPics.map(function(image, imageIndex){
+      return <img key={ imageIndex } src={ image } />
+
+    })}
+    */}
+
+    <h1>Welcome to Yadav's Family Homepage.</h1>
+    <p>Check Family Details Section for more details.</p>
+
+    {/*
+    <img
+      src={pics/family.jpg}
+      alt="Yadav Family"
+      style={{ borderColor: 'black' }}
+    />
+    */}
+    
+
+    <div className="wrapper">
+      <div className="item">
+        <img
+          src="pics/family.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src="pics/yadav_family.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src="pics/family1.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src="pics/family2.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src="pics/family3.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src="pics/family4.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src="pics/family5.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src="pics/family6.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src="pics/family7.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>      
+      <div className="item">
+        <img
+          src="pics/familyPic1.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>      
+      <div className="item">
+        <img
+          src="pics/kids.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>      
+      <div className="item">
+        <img
+          src="pics/family8.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src="pics/yadav_family_2.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src="pics/yadav_family_3.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>      
+      <div className="item">
+        <img
+          src="pics/familyFriends3.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src="pics/familyFriends1.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src="pics/familyFriends2.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src="pics/yadav_family_hierarchy.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+    </div>  
+
+
+     {/*
+     <div className="item">
+        <img
+          src="pics/yadav_family_1.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>         
+      <div className="item">
+        <img
+          src="pics/siblings.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div> 
+     */}   
+    
+  </div>
+)
